429c7d345e0ad6b54b92b1883f165a0bd1645a35
SimonDierickx <simon.dierickx@student.hogent.be>
2024-10-24 14:15:56 +0200

    kaas

--- startup.sh
+++ startup.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
+echo kaas1
 
-# Update the package index
-apt-get update -y
-
-# Install Docker and wget using apt-get
-apt-get install -y docker.io wget
-
-# Start and enable Docker
-systemctl start docker
-systemctl enable docker
-
-# Pull the latest SQL Server Docker image
 docker pull mcr.microsoft.com/mssql/server:2022-latest
-
-# Run the SQL Server container
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=YourStrongPassw0rd" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2022-latest
-
-# Download .NET SDK for the appropriate architecture
+echo kaas2
+docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" -p 1433:1433 --name sql1 --hostname sql1 -d  mcr.microsoft.com/mssql/server:2022-latest
+echo kaas3
 wget https://download.visualstudio.microsoft.com/download/pr/18e32a84-60ec-4d82-8ab1-84511be4172b/4a1e6bdd4f15e0d55e0d9bb20c67631e/dotnet-sdk-8.0.403-linux-musl-arm64.tar.gz
-
-# Create directory for dotnet and extract the SDK
+echo kaas4
 mkdir -p $HOME/dotnet
 tar zxf dotnet-sdk-8.0.403-linux-musl-arm64.tar.gz -C $HOME/dotnet
-
-# Set environment variables for .NET SDK
+echo kaas5
 export DOTNET_ROOT=$HOME/dotnet
-export PATH=$PATH:$HOME/dotnet
-
-# Navigate to the project directory
-cd p3ops-demo-app
-
-# Clean, restore, build, and publish the application
+export DOTNET_ROOT=$HOME/dotnet
+echo kaas6
 dotnet clean
+echo kaas7
 dotnet restore src/Server/Server.csproj
+echo kaas8
 dotnet build src/Server/Server.csproj
+echo kaas9
 dotnet publish src/Server/Server.csproj -c Release -o publish
+echo kaas10
 
-# Set environment variables for the application
 export DOTNET_ENVIRONMENT=Production
 export DOTNET_ConnectionStrings__SqlDatabase="Server=localhost,1433;Database=SportStore;User Id=sa; Password=YourStrongPassw0rd;TrustServerCertificate=True;"
-
-# Run the application
+echo kaas11
 dotnet publish/Server.dll
+echo kaas12
