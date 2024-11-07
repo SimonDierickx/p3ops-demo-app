246837b4f60f2079c89ee6d617d0a4e834d1b4ca
SimonDierickx <simon.dierickx@student.hogent.be>
2024-11-07 13:35:01 +0100

    test db

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -1,94 +1,38 @@
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Persistence;
-using Persistence.Triggers;
-using Services.Products;
-using Shared.Products;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // Add this for MySQL options
 
-namespace Server
+public void ConfigureServices(IServiceCollection services)
 {
-    public class Startup
+    services.AddDbContext<SportStoreDbContext>(options =>
     {
-        public IConfiguration Configuration { get; }
-        public IWebHostEnvironment Environment { get; }
+        options.UseMySql(
+            Configuration.GetConnectionString("SqlDatabase"),
+            new MySqlServerVersion(new Version(8, 0, 21)) // specify your MySQL version here
+        );
 
-        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        if (Environment.IsDevelopment())
         {
-            Configuration = configuration;
-            Environment = environment;
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<SportStoreDbContext>(options =>
+            options.EnableDetailedErrors();
+            options.EnableSensitiveDataLogging();
+            options.UseTriggers(triggers =>
             {
-                options.UseNpgsql(Configuration.GetConnectionString("SqlDatabase"));
-                if (Environment.IsDevelopment())
-                {
-                    options.EnableDetailedErrors();
-                    options.EnableSensitiveDataLogging();
-                    options.UseTriggers(triggers =>
-                    {
-                        triggers.AddTrigger<OnBeforeEntitySaved>();
-                    });
-                }
-            });
-
-            // Configure FluentValidation on IServiceCollection, not on IMvcBuilder
-            services.AddFluentValidationAutoValidation();
-            services.AddFluentValidationClientsideAdapters();
-            services.AddValidatorsFromAssembly(typeof(ProductDto.Mutate.Validator).Assembly);
-
-            services.AddControllersWithViews();
-            services.AddSwaggerGen(c =>
-            {
-                c.CustomSchemaIds(x => $"{x.DeclaringType.Name}.{x.Name}");
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sportstore API", Version = "v1" });
-            });
-
-            services.AddRazorPages();
-            services.AddProductServices();
-            services.AddScoped<SportStoreDataInitializer>();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SportStoreDataInitializer dataInitializer)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseWebAssemblyDebugging();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sportstore API"));
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-
-            dataInitializer.SeedData();
-
-            app.UseHttpsRedirection();
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-                endpoints.MapFallbackToFile("index.html");
+                triggers.AddTrigger<OnBeforeEntitySaved>();
             });
         }
-    }
+    });
+
+    // Other service configurations...
+    services.AddFluentValidationAutoValidation();
+    services.AddFluentValidationClientsideAdapters();
+    services.AddValidatorsFromAssembly(typeof(ProductDto.Mutate.Validator).Assembly);
+
+    services.AddControllersWithViews();
+    services.AddSwaggerGen(c =>
+    {
+        c.CustomSchemaIds(x => $"{x.DeclaringType.Name}.{x.Name}");
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sportstore API", Version = "v1" });
+    });
+
+    services.AddRazorPages();
+    services.AddProductServices();
+    services.AddScoped<SportStoreDataInitializer>();
 }
