c3368855ac53e71dc8f1f44c213a7a73cf7499a8
SimonDierickx <simon.dierickx@student.hogent.be>
2024-11-07 13:40:48 +0100

    startup

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -1,38 +1,66 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // Add this for MySQL options
+using Persistence;
+using Persistence.Triggers;
+using Services.Products;
+using Shared.Products;
 
-public void ConfigureServices(IServiceCollection services)
+namespace Server
 {
-    services.AddDbContext<SportStoreDbContext>(options =>
+    public class Startup
     {
-        options.UseMySql(
-            Configuration.GetConnectionString("SqlDatabase"),
-            new MySqlServerVersion(new Version(8, 0, 21)) // specify your MySQL version here
-        );
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
-        if (Environment.IsDevelopment())
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
-            options.EnableDetailedErrors();
-            options.EnableSensitiveDataLogging();
-            options.UseTriggers(triggers =>
-            {
-                triggers.AddTrigger<OnBeforeEntitySaved>();
-            });
+            Configuration = configuration;
+            Environment = environment;
         }
-    });
 
-    // Other service configurations...
-    services.AddFluentValidationAutoValidation();
-    services.AddFluentValidationClientsideAdapters();
-    services.AddValidatorsFromAssembly(typeof(ProductDto.Mutate.Validator).Assembly);
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SportStoreDbContext>(options =>
+            {
+                options.UseMySql(
+                    Configuration.GetConnectionString("SqlDatabase"),
+                    new MySqlServerVersion(new Version(8, 0, 21)) // specify your MySQL version here
+                );
 
-    services.AddControllersWithViews();
-    services.AddSwaggerGen(c =>
-    {
-        c.CustomSchemaIds(x => $"{x.DeclaringType.Name}.{x.Name}");
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sportstore API", Version = "v1" });
-    });
+                if (Environment.IsDevelopment())
+                {
+                    options.EnableDetailedErrors();
+                    options.EnableSensitiveDataLogging();
+                    options.UseTriggers(triggers =>
+                    {
+                        triggers.AddTrigger<OnBeforeEntitySaved>();
+                    });
+                }
+            });
 
-    services.AddRazorPages();
-    services.AddProductServices();
-    services.AddScoped<SportStoreDataInitializer>();
+            // Configure FluentValidation on IServiceCollection
+            services.AddFluentValidationAutoValidation();
+            services.AddFluentValidationClientsideAdapters();
+            services.AddValidatorsFromAssembly(typeof(ProductDto.Mutate.Validator).Assembly);
+
+            services.AddControllersWithViews();
+            services.AddSwaggerGen(c =>
+            {
+                c.CustomSchemaIds(x => $"{x.DeclaringType.Name}.{x.Name}");
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sportstore API", Version = "v1" });
+            });
+
+            services.AddRazorPages();
+            services.AddProductServices();
+            services.AddScoped<SportStoreDataInitializer>();
+        }
+    }
 }
